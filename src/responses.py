1f30f7e1f8e1982150ec36c0fdf3ad916346aa6a
cocomine <roguish.6888467468@gmail.com>
2023-02-17 21:20:55 +0800

    fix block

--- src/responses.py
+++ src/responses.py
@@ -3,6 +3,9 @@ from revChatGPT.V1 import Chatbot
 from dotenv import load_dotenv
 import os
 from src import log
+import functools
+import typing
+import asyncio
 
 logger = log.setup_logger(__name__)
 
@@ -15,6 +18,19 @@ chatbot = Chatbot(config={
 })
 
 
+def thread_ask(func: typing.Callable) -> typing.Coroutine:
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        return await asyncio.to_thread(func, *args, **kwargs)
+
+    return wrapper
+
+
+@thread_ask
+def ask(prompt):
+    return chatbot.ask(prompt)
+
+
 async def handle_response(user_message: str, message: discord.Message, isreplyall=False,
                           channel: discord.TextChannel = None):
     prev_msg = ""
@@ -22,16 +38,17 @@ async def handle_response(user_message: str, message: discord.Message, isreplyal
     author = ""
 
     if message is not None:
-        author = "<@"+str(message.author.id)+">"
+        author = "<@" + str(message.author.id) + ">"
 
     if channel is None:
         channel = message.channel
 
     async with channel.typing():
-        for data in chatbot.ask(user_message):
+        respone = await ask(user_message)
+        for data in respone:
             output_msg = data["message"][len(prev_msg):]  # get response
 
-            if len(output_msg) > 0:
+            if len(output_msg) > 1900:
                 if my_message is None:
                     if message is None and channel is not None:
                         my_message = await channel.send(output_msg)
@@ -41,8 +58,8 @@ async def handle_response(user_message: str, message: discord.Message, isreplyal
                         else:
                             my_message = await message.followup.send(output_msg)
                 else:
-                    if len(output_msg) >= 1999:
-                        prev_msg = output_msg
+                    if len(output_msg) > 1900:
+                        output_msg = output_msg[len(prev_msg):]
 
                         if message is None and channel is not None:
                             my_message = await channel.send(output_msg)
@@ -52,6 +69,7 @@ async def handle_response(user_message: str, message: discord.Message, isreplyal
                             else:
                                 my_message = await message.followup.send(output_msg)
                     else:
+                        prev_msg = output_msg
                         await my_message.edit(content=output_msg)
 
     logger.info(f"\x1b[31m{author}\x1b[0m : '{user_message}' ({str(channel)}) finish respond")
